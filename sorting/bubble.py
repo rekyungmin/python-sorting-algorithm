087c3373e9befd844daaeb85fc4b7b62f3ab86a6
Kyungmin <rekyungmin@gmail.com>
2020-07-12 21:17:01 +0900

    feat: add an improved bubble-sort algorithm

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -2,6 +2,7 @@ from typing import List, TypeVar
 
 __all__ = (
     "bubble_sort",
+    "bubble_sort_imp",
 )
 
 T = TypeVar("T")
@@ -23,4 +24,23 @@ def bubble_sort(
                 is_compare = True
         if not is_compare:
             break
+    return data
+
+
+def bubble_sort_imp(
+    data: List[T], *, reverse: bool = False, inplace: bool = False
+) -> List[T]:
+    if not inplace:
+        data = data.copy()
+
+    comparison_op: str = "__lt__" if reverse else "__gt__"
+    unsorted_max_index: int = len(data) - 1
+    last_unsorted_max_index: int = -1
+
+    while unsorted_max_index != last_unsorted_max_index:
+        last_unsorted_max_index = unsorted_max_index
+        for i in range(unsorted_max_index):
+            if getattr(data[i], comparison_op)(data[i + 1]):
+                data[i], data[i + 1] = data[i + 1], data[i]
+                unsorted_max_index = i
     return data
